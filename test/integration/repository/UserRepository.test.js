baf6849ea0f0f627d60200bf658bdbb0f57e8226
JosueDAVID2309 <josue.ataulluco.david@gmail.com>
2026-09-05 17:03:04 -0500

    Algunos cambios

--- test/integration/repository/UserRepository.test.js
+++ test/integration/repository/UserRepository.test.js
@@ -1,19 +1,20 @@
 const repository = require("../../../src/infrastructure/repositories/UserRepository");
 
 describe("Test de los metodos de repository", () => {
+
+  test("El email ingresado debe existir en la base de datos", async () => {
+    const result = await repository.existsByEmail("carlos.gomez@mail.com");
+    expect(result).toBe(true);
+  });
+
   test("El email ingresado no debe existir en la base de datos", async () => {
     const result = await repository.existsByEmail("randomEmail@example.com");
     expect(result).toBe(false);
   });
 
-  test("El email ingresado debe existir en la base de datos", async () => {
-    const result = await repository.existsByEmail("mgomez@citasmedicas.com");
-    expect(result).toBe(true);
-  });
-
   test("Debe retornar un usuario", async () => {
     const result = await repository.findUsuariobyEmail(
-      "luis.infantes@email.com",
+      "luis.ramirez@mail.com",
     );
     expect(result != null).toBe(true);
   });
@@ -22,6 +23,6 @@ describe("Test de los metodos de repository", () => {
     const result = await repository.findUsuariobyEmail(
       "randomEmail@example.com",
     );
-    expect(result != null).toBe(false);
+    expect(result).toBe(null);
   });
 });
