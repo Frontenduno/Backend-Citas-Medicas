2b17f4f7f22a9a90337831a722e9f22613d7265c
JosueDavid <josue.ataulluco.david@gmail.com>
2026-09-10 09:12:11 -0500

    Aun refactorizando

---
+++ src/application/exception/CorreoRegistradoException.js
@@ -0,0 +1,11 @@
+class CorreoRegistradoException{
+
+    constructor(){
+        super("Este correo ya existe en esta plataforma...");
+    }
+
+}
+
+module.exports = {
+    CorreoRegistradoException
+}
